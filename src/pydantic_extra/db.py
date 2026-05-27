d12b5c6cd8efa28d5066ccccf325b11f97cc677b
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-05-27 18:05:46 +0300

    reformatting

--- src/pydantic_extra/db.py
+++ src/pydantic_extra/db.py
@@ -1,24 +1,17 @@
 __all__ = ["DB", "SQLite", "Mysql", "AnyDB", "T_DB"]
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from functools import cached_property
-from pathlib import Path
 
-from pydantic import BaseModel, Field, SecretStr
+from pydantic import BaseModel
 from sqlalchemy import URL, Engine, create_engine, event
-from sqlalchemy.engine.interfaces import DBAPIConnection
 from sqlalchemy.orm import Session
-from typing_extensions import Literal, TypeAlias
+from typing_extensions import TypeAlias
+
+from pydantic_extra._db import AnyBase, DBBase, MysqlBase, SQLiteBase
 
 
-class DB(BaseModel, ABC):
-    type: str
-
-    @cached_property
-    @abstractmethod
-    def connect_str(self) -> str | URL:
-        """строка для sqlalchemy.create_engine"""
-
+class DB(BaseModel, DBBase, ABC):
     @cached_property
     def engine(self) -> Engine:
         """Создаёт sqlalchemy.Engine"""
@@ -33,68 +26,24 @@ class DB(BaseModel, ABC):
         return engine
 
 
-class SQLite(DB):
-    type: Literal["sqlite"]
-    path: Path
-
+class SQLite(DB, SQLiteBase):
     @cached_property
     def connect_str(self) -> str | URL:
         """строка для sqlalchemy.create_engine"""
         return URL.create("sqlite", database=str(self.path.absolute()))
 
     def setup(self, engine: Engine) -> Engine:
-        """Настраивает экземпляр sqlalchemy.Engine для работы с данным диалектом"""
-
-        @event.listens_for(engine, "connect")
-        def set_sqlite_pragma(dbapi_connection: DBAPIConnection, connection_record):
-            cursor = dbapi_connection.cursor()
-            cursor.execute("PRAGMA foreign_keys=ON")
-            dbapi_connection.commit()
-            cursor.close()
-
+        """Настраивает экземпляр sqlalchemy.Engine для работы с sqlite"""
+        event.listen(engine, "connect", self._set_pragma)
         return engine
 
 
-class Mysql(DB):
-    type: Literal["mysql", "mariadb"]
-    host: str
-    port: int = 3306
-    login: str
-    password: SecretStr
-    encoding: str = "utf8mb4"
-    database: str
-    _library: str = "pymysql"
-
-    @cached_property
-    def connect_str(self) -> str | URL:
-        """строка для sqlalchemy.create_engine"""
-        return URL.create(
-            f"mysql+{self._library}",
-            self.login,
-            self.password.get_secret_value(),
-            self.host,
-            self.port,
-            self.database,
-            {"charset": self.encoding},
-        )
-
-    @property
-    def library(self) -> str:
-        return self._library
-
-    @library.setter
-    def library(self, value: str):
-        self._library = value
+class Mysql(DB, MysqlBase, default_library="pymysql"):
+    pass
 
 
-class AnyDB(DB):
-    type: Literal["any"]
-    str_: str = Field(alias="str")
-
-    @cached_property
-    def connect_str(self) -> str | URL:
-        """строка для sqlalchemy.create_engine"""
-        return self.str_
+class AnyDB(DB, AnyBase):
+    pass
 
 
 T_DB: TypeAlias = SQLite | Mysql | AnyDB
