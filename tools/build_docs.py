f2ad826ad53031f2dc577aba4043f658fa127a85
zedzhen <59135268+zedzhen@users.noreply.github.com>
2026-06-22 11:25:29 +0300

    update tools

--- tools/build_docs.py
+++ tools/build_docs.py
@@ -1,13 +1,14 @@
 from subprocess import run
 
 from __tools_init import chdir
-from _const import build_dir, docs_dir
+from _const import docs_dir
+from _docs import args, build_dir, lang_from_argv
 
 
-def main():
+def main(lang: str | None) -> None:
     with chdir:
-        run(["sphinx-build", docs_dir, build_dir])
+        run(["sphinx-build", docs_dir, build_dir(lang), *args(lang)])
 
 
 if __name__ == "__main__":
-    main()
+    main(lang_from_argv())
